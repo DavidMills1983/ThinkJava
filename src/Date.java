be85c1ef317a6faf344e89aa90d48a65b0877e22
David Mills <millsdg-gmail.com>
2024-11-03 17:56:48 +0000

    first version

--- src/Date.java
+++ src/Date.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Date {
+    private int year, month, day;
+
+    public Date(){
+        this.day = 3;
+        this.month = 11;
+        this.year = 2024;
+    }
+
+    public Date(int year, int month, int day){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Your birthday is: " + day + "/" + month + "/ " + year;
+    }
+
+    public static void main(String[] args) {
+        Date birthday = new Date(1983, 7, 14);
+        System.out.println(birthday);
+
+
     }
 }
